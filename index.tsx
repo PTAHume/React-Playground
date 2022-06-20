a35aaaf5e5ca1b3f9ff83644847ee5201012e3bf
PTAHume <patricktahume@gmail.com>
2022-06-20 13:11:31 +0100

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,41 +1,14 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.css';
-//import App from './App';
+
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-//const App = ({ title }) => <div className="header">{title}</div>;
-
-class Car extends React.Component {
-  render(){
-    return (
-      <div className="github-profile">
-        <img src="https://placehold.it/75" />
-        <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
-        </div>
-      </div>
-    )
-  }
-}
-
-class App extends React.Component {
-  render(){
-    return (
-      <div>
-        <div className="header">{this.props.title}</div>
-        <Car/>
-      </div>
-    )
-  }
-}
-
 root.render(
   <StrictMode>
-    <App title="The GitHub Cards App" />
+    <App />
   </StrictMode>
 );
