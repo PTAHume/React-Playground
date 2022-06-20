b2d2898d037866f06ab6db427317e1494358f1e0
PTAHume <102745939+PTAHume@users.noreply.github.com>
2022-06-21 00:12:59 +0100

    dev 2

--- src/App.tsx
+++ src/App.tsx
@@ -21,19 +21,21 @@ const testData = [
 
 const CardList = (props: any) => (
   <div>
-    <Card />
+    {testData.map((profile) => (
+      <Card {...profile} />
+    ))}
   </div>
 );
 
-class Card extends React.Component {
+class Card extends React.Component<any> {
   render() {
-    const profile = testData[0];
+    const profile = this.props;
     return (
       <div className="github-profile" style={{ margin: "1rem" }}>
         <img title="place-hold" alt="Missing" src={profile.avatar_url} />
         <div className="info" style={{ display: "inline-block", margin: 10 }}>
           <div className="name" style={{ fontSize: "125%" }}>
-            {profile.avatar_url}
+            {profile.name}
           </div>
           <div className="company">{profile.company}</div>
         </div>
